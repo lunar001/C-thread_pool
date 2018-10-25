9d69766c39ae02c571397ab8c620e3a91329c8f1
lunar001 <214607868@qq.com>
2018-10-25 22:43:21 +0800

    add the code of threadpool

---
+++ ThreadPool.cpp
@@ -0,0 +1,42 @@
+#include "ThreadPool.h"
+#include "CPPThread.h"
+
+void ThreadPool::work_()
+{
+	while(!tasks_.empty())/* if tasks_ is empty, waiting at here*/
+	{
+		Thread::Func task;
+		tasks_.wait_and_pop(task);
+		task();
+	}
+}
+
+void ThreadPool::put_task(const Thread::Func & task)
+{
+	tasks_.push(task);
+}
+
+void ThreadPool::start(int num) 
+{
+	/* num is the num of thread */
+	if (num != 0)
+		threadNum_ = num;
+	else
+		threadNum_ = 4; /* create 4 threads in thread pool now*/
+
+	for ( int i = 0; i < threadNum_; i++)
+	{
+		/* make_shared need class has a copy constructor or move constructor*/
+		/*std::shared_ptr<Thread> tp = std::make_shared<CPPThread>(
+				CPPThread(std::bind(&ThreadPool::work_, this)));*/
+		std::shared_ptr<Thread> tp(new CPPThread(std::bind(&ThreadPool::work_, this)));
+		threads_.push_back(tp);
+		tp->startThread();
+	}
+}
+
+void ThreadPool::stop()
+{
+	for (auto tp : threads_)
+		tp->join();
+}
--- ThreadPool.h
+++ ThreadPool.h
@@ -1,13 +1,23 @@
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
 #include "Thread.h"
+#include "ThreadQueue.h"
 #include <vector>
 
 class ThreadPool
 {
 public:
-	
-private:
-	std::vector<std::shared_ptr<Thread>> threads_;
-	std::vector<:
-	int threadNum_;
+	ThreadPool():threadNum_(0){};
 
+	void put_task(const Thread::Func & task);/* push task into threadpool */
+
+	void start(int num = 0); 
+	void stop(); /* try to stop all the threads */
+
+private:
+	std::vector<std::shared_ptr<Thread>> threads_; /* The number of thread is limits*/
+	int threadNum_;
+	ThreadQueue<Thread::Func> tasks_;
+	void work_(); /* The work func of ths threadpool*/
 };
+#endif 
